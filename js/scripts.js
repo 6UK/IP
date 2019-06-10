92909c650ff32eda5ffff295b4d8b45ba2eb5af8
6UK <karugunjoroge@gmail.com>
2019-06-10 13:44:15 +0300

    added a paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,47 @@
+$(document).ready(function(){
+$(".state1").click(function(){
+$(".state1").hide();
+$(".design").show();
+$(".arm1").toggle();
+});
 
+$(".arm1").click(function(){
+$(".state1").show();
+$(".arm1").toggle();
+});
+
+
+
+$("#state2").click(function(){
+$("#state2").hide();
+$("#development").show();
+$(".arm2").toggle();
+});
+
+$(".arm2").click(function(){
+$("#state2").show();
+$(".arm2").toggle();
+});
+
+
+
+$("#state3").click(function(){
+$("#state3").hide();
+$("#development").show();
+$(".arm3").toggle();
+});
+
+$(".arm3").click(function(){
+$("#state3").show();
+$(".arm3").toggle();
+});
+});
+
+
+
+function returnValue()
+{
+   var name = document.getElementById("name").value;
+ alert(name + ' message recieved! Thank you For contacting us!');
+ return false;
+}
